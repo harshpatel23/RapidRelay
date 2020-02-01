34bd900fd591aa6ab199f320f2c8d697ec3c23a1
murtaza98 <m.patrawala@somaiya.edu>
2020-02-01 20:33:51 +0530

    sync

--- subscriber_agriculture.py
+++ subscriber_agriculture.py
@@ -1,17 +1,9 @@
 import paho.mqtt.client as mqtt  # import the client1
 import time
 import json
-import mysql.connector as mysql
 
-
-# Database connection
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "",
-    database = ""
-)
-
+broker_address = "10.0.15.92"
+queue_name = "queue_agriculture"
 
 def on_message(client, userdata, message):
     print("message received ", str(message.payload.decode("utf-8")))
@@ -23,20 +15,18 @@ def on_message(client, userdata, message):
     print(str(msg))
 
 
+def on_connect(mqtt_client, obj, flags, rc):
+    print("Subscribing to topic ", queue_name)
+    mqtt_client.subscribe(queue_name)
 
-
-broker_address = "10.0.15.92"
-queue_name = "queue_agriculture"
 
 print("creating new instance")
 client = mqtt.Client("P2")  # create new instance
 client.on_message = on_message  # attach function to callback
+client.on_connect = on_connect
 print("connecting to broker")
-
 client.connect(broker_address, port=1883)  # connect to broker
-client.loop_start()  # start the loop
-print("Subscribing to topic ", queue_name)
-client.subscribe(queue_name)
 
-time.sleep(4)  # wait
+client.loop_forever()
+# time.sleep(4)  # wait
 # client.loop_stop()  # stop the loop
