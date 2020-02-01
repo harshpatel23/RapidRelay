3276058625ab71bde31ebb4926a1c5596d64227c
ojasskapre <ojas.kapre@somaiya.edu>
2020-02-02 02:33:44 +0530

    cloud recieve insert into db added

--- cronjobpy.py
+++ cronjobpy.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user='tanay')
-job = cron.new(command='/home/tanay/Projects/BugBox/upload_data.py >> /var/log/myjob.log 2>&1')
-job.minute.every(1)
+job = cron.new(command='/home/tanay/Projects/BugBox/upload_data.py >> /home/tanay/Projects/BugBox/logs.txt 2>&1')
+job.minute.every(5)
 
 cron.write()
--- upload_data.py
+++ upload_data.py
@@ -1,4 +1,4 @@
-#!/home/tanay/Projects/BugBox/venv/bin/python
+#!/home/tanay/Projects/BugBox/venv1/bin/python
 
 import pymysql
 import socket
@@ -11,151 +11,153 @@ from threading import Thread
 import socket
 import os
 
-host = '127.0.0.1'  # cloud IP
+host = 'www.google.com'  # cloud IP
 server = 'localhost'
 username = 'root'
 password = ''
 database = 'bugbox_db'
 cursorclass = pymysql.cursors.DictCursor
+base_dir = '/home/tanay/Projects/BugBox'
 compressed_files_dir = 'compressed_files'
 raw_files_dir = 'raw_files'
 cloud_url = ''
 
 
 def is_connected(hostname):
-	try:
-		# see if we can resolve the host name -- tells us if there is
-		# a DNS listening
-		host = socket.gethostbyname(hostname)
-		# connect to the host -- tells us if the host is actually
-		# reachable
-		s = socket.create_connection((host, 80), 2)
-		s.close()
-		return True
-	except Exception as e:
-		traceback.print_exc()
-	return False
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(hostname)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        s = socket.create_connection((host, 80), 2)
+        s.close()
+        return True
+    except Exception as e:
+        traceback.print_exc()
+    return False
 
 
 def compress_data_to_file_from(table):
-	print('compressing', table)
-	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
-	cursor = db.cursor()
+    print('compressing', table)
+    db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
+    cursor = db.cursor()
 
-	# 1. extract data
-	query = f'select * from {table};'
-	# print(query)
-	try:
-		cursor.execute(query)
-		results = cursor.fetchall()
-		if not results:
-			return
+    # 1. extract data
+    query = f'select * from {table};'
+    # print(query)
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        if not results:
+            return
 
-		# print(results)
-		ids = [x['id'] for x in results]
-		# print(ids)
-		# print('after')
+        # print(results)
+        ids = [x['id'] for x in results]
+        # print(ids)
+        # print('after')
 
-		# 2. compress and encode data
-		results = json.dumps(results)
-		results = bytes(results, 'utf-8')
-		current_time = str(time.time()).replace('.', '_')
-		with open(f'{raw_files_dir}/{table}_{current_time}', 'wb') as bin_file:
-			bin_file.write(results)
-
-
-		# print('json data', results)
-		compressed = base64.b64encode(zlib.compress(results, 9))
-		# print(compressed, type(compressed))
-
-		with open(f'{compressed_files_dir}/{table}_{current_time}', 'wb') as bin_file:
-			bin_file.write(compressed)
+        # 2. compress and encode data
+        results = json.dumps(results)
+        results = bytes(results, 'utf-8')
+        current_time = str(time.time()).replace('.', '_')
+        with open(f'{base_dir}/{raw_files_dir}/{table}_{current_time}', 'wb') as bin_file:
+            bin_file.write(results)
 
 
-		# 3. delete data from db
+        # print('json data', results)
+        compressed = base64.b64encode(zlib.compress(results, 9))
+        # print(compressed, type(compressed))
 
-		ids = ", ".join([str(x) for x in ids])
-		delete_query = f'delete from {table} where id in ({ids});'
-		# print(delete_query)
-		cursor.execute(delete_query)
-		db.commit()
+        with open(f'{base_dir}/{compressed_files_dir}/{table}_{current_time}', 'wb') as bin_file:
+            bin_file.write(compressed)
 
-	except Exception as e:
-		traceback.print_exc()
-		db.rollback()
 
-	db.close()
+        # 3. delete data from db
+
+        ids = ", ".join([str(x) for x in ids])
+        delete_query = f'delete from {table} where id in ({ids});'
+        # print(delete_query)
+        cursor.execute(delete_query)
+        db.commit()
+
+    except Exception as e:
+        traceback.print_exc()
+        db.rollback()
+
+    db.close()
 
 
 def send_data_to_cloud(filename):
-	s = socket.socket()             # Create a socket object
-	host = socket.gethostname()     # Get local machine name
-	port = 60000                    # Reserve a port for your service.
-	buffer_size = 102400
+    s = socket.socket()             # Create a socket object
+    host = socket.gethostname()     # Get local machine name
+    port = 60000                    # Reserve a port for your service.
+    buffer_size = 102400
 
-	s.connect((host, port))
+    s.connect((host, port))
 
-	f = open(filename,'rb')
-	l = f.read(buffer_size)
-	while (l):
-	   s.send(l)
-	   # print('Sent ',repr(l))
-	   l = f.read(buffer_size)
-	f.close()
+    f = open(f'{base_dir}/{compressed_files_dir}/{filename}','rb')
+    l = f.read(buffer_size)
+    while (l):
+        s.send(l)
+        # print('Sent ',repr(l))
+        l = f.read(buffer_size)
+    f.close()
 
-	print('Done sending'+filename)
-	s.close()
-	f.close()
-	s.close()
+    print('Done sending'+filename)
+    s.close()
+    f.close()
+    s.close()
 
 
 def is_already_running():
-	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
-	cursor = db.cursor()
+    db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
+    cursor = db.cursor()
 
-	query = 'select value from locks where flag = 1;'
-	cursor.execute(query)
-	results = cursor.fetchall()
+    query = 'select value from locks where flag = 1;'
+    cursor.execute(query)
+    results = cursor.fetchall()
 
-	if results[0]['value'] == 'false':
-		return False
-	else:
-		return True
+    if results[0]['value'] == 'false':
+        return False
+    else:
+        return True
 
 
 Threads = []
 for table in ['agriculture_data', 'air_quality_data', 'weather_data']:
-	process = Thread(target=compress_data_to_file_from, args=[table])
-	process.start()
-	Threads.append(process)
+    process = Thread(target=compress_data_to_file_from, args=[table])
+    process.start()
+    Threads.append(process)
 
 # check internet connection
 if is_connected(host) and not is_already_running():
 
-	# set flag
-	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
-	cursor = db.cursor()
-	query = 'update locks set value = "true" where flag = 1;'
-	cursor.execute(query)
-	db.commit()
+    # set flag
+    db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
+    cursor = db.cursor()
+    query = 'update locks set value = "true" where flag = 1;'
+    cursor.execute(query)
+    db.commit()
 
-	# if connected, send files one by one to the cloud
+    # if connected, send files one by one to the cloud
 
-	print('connected')
-
-	for filename in os.listdir(compressed_files_dir):
-		print(filename)
-		# send file to cloud
-		# send_data_to_cloud(compressed_files_dir+'/'+ filename)
-
-	# flag off
-	query = 'update locks set value = "false" where flag = 1;'
-	cursor.execute(query)
-	db.commit()
-
-	# TODO remove compressed files after use
+    print('connected')
+    try:
+        for filename in os.listdir(f'{base_dir}/{compressed_files_dir}'):
+            print(filename)
+            # send file to cloud
+            send_data_to_cloud(compressed_files_dir+'/'+ filename)
+            # TODO remove compressed files after use
+    except Exception as e:
+        traceback.print_exc()
+    finally:
+        # flag off
+        query = 'update locks set value = "false" where flag = 1;'
+        cursor.execute(query)
+        db.commit()
 
 else:
-	# else don't do anything
-	print('not connected or already running')
-	pass
+    # else don't do anything
+    print('not connected or already running')
+    pass
