94044e7a528c07aa3036dfc29b02335cc4164544
Harsh Patel <harsh.patel4@somaiya.edu>
2020-02-02 06:17:17 +0530

    ui

--- frontend.py
+++ frontend.py
@@ -1,17 +1,35 @@
 from flask import Flask, render_template
+import pymysql
+
 app = Flask(__name__)
+server = 'localhost'
+username = 'root'
+password = ''
+database = 'bugbox_db'
+cursorclass = pymysql.cursors.DictCursor
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+	return render_template('base.html')
 
 
-@app.route('/weather/<city>')
+@app.route('/weather/<city>/', methods=['GET'])
 def show_city_weather(city):
-    # query database
-    return None
+	# query database
+	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
+	cursor = db.cursor()
+	sql = 'SELECT * FROM weather_data where city="'+city+'" SORT BY TIME DESC LIMIT 10;'
+	try:
+		cursor.execute(sql)
+		results = cursor.fetchall()
+	except Exception as e:
+		traceback.print_exc()
+		db.rollback()
+
+	db.close()
+	return render_template("tables.html", context={'data': results})
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run(debug= True)
