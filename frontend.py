9db659699325b931d3c2aa513eb40a92a2bc5036
murtaza98 <m.patrawala@somaiya.edu>
2020-02-02 08:26:12 +0530

    Web UI completed

--- frontend.py
+++ frontend.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import pymysql
+import traceback
+
 
 app = Flask(__name__)
 server = 'localhost'
@@ -19,8 +21,14 @@ def show_city_weather(city):
 	# query database
 	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
 	cursor = db.cursor()
-	sql = 'SELECT * FROM weather_data where city="'+city+'" SORT BY TIME DESC LIMIT 10;'
+	count_sql = 'SELECT COUNT(*) as c FROM weather_data WHERE city="'+city+'";'
+
+	sql = 'SELECT * FROM weather_data where city="'+city+'" ORDER BY `TIME` DESC LIMIT 10;'
+	print(sql)
 	try:
+		cursor.execute(count_sql)
+		counts = cursor.fetchone()
+		print(counts['c'])
 		cursor.execute(sql)
 		results = cursor.fetchall()
 	except Exception as e:
@@ -28,8 +36,50 @@ def show_city_weather(city):
 		db.rollback()
 
 	db.close()
-	return render_template("tables.html", context={'data': results})
+	return render_template("tables.html", data=results, count=counts['c'])
+
+
+@app.route('/agriculture/', methods=['GET'])
+def show_greehouse_data():
+	# query database
+	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
+	cursor = db.cursor()
+	count_sql = 'SELECT COUNT(*) as c FROM agriculture_data;'
+	sql = 'SELECT * FROM agriculture_data ORDER BY `TIME` DESC LIMIT 10;'
+	print(sql)
+	try:
+		cursor.execute(count_sql)
+		counts = cursor.fetchone()
+		cursor.execute(sql)
+		results = cursor.fetchall()
+	except Exception as e:
+		traceback.print_exc()
+		db.rollback()
+
+	db.close()
+	return render_template("tables.html", data=results, count=counts['c'])
+
+
+@app.route('/air_quality/<city>/', methods=['GET'])
+def show_city_air_quality(city):
+	# query database
+	db = pymysql.connect(server, username, password, database, cursorclass=cursorclass)
+	cursor = db.cursor()
+	count_sql = 'SELECT COUNT(*) as c FROM air_quality_data WHERE city="' + city + '";'
+	sql = 'SELECT * FROM air_quality_data where city="'+city+'" ORDER BY `TIME` DESC LIMIT 10;'
+	print(sql)
+	try:
+		cursor.execute(count_sql)
+		counts = cursor.fetchone()
+		cursor.execute(sql)
+		results = cursor.fetchall()
+	except Exception as e:
+		traceback.print_exc()
+		db.rollback()
+
+	db.close()
+	return render_template("tables.html", data=results, count=counts['c'])
 
 
 if __name__ == '__main__':
-	app.run(debug= True)
+	app.run(debug=True)
